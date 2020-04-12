85418df41fec9a986661c418935e12d73b014195
Anuar <anuar.ssn@gmail.com>
2020-04-12 13:31:34 +0300

    helped test file

--- tests/digitalRootTest.php
+++ tests/digitalRootTest.php
@@ -5,32 +5,40 @@ use digitalRootSrc\digitalRootBuilder;
 
 final class digitalRootTest extends TestCase {
     public function testDigitalRoot() : void {
-        $this->assertEquals("2", (new digitalRootBuilder)->getDigitalRoot("23081996"));
+        $this->assertEquals([
+            'client_input' => '23081996',
+            'digital_root' => '2'
+        ], (new digitalRootBuilder)->getDigitalRoot('23081996'));
     }
 
     public function testDigitalRootCompleteCalculation() : void {
         $this->assertEquals([
-            'root' => '2',
+            'client_input' => '23081996',
+            'digital_root' => '2',
             'digits' => [2,5,5,4,5,5,5,2],
             'numeric' => '25545552'
-        ], 
-        (new digitalRootBuilder)->getDigitalRootCompleteCalculation("23081996"));
+        ], (new digitalRootBuilder)->getDigitalRootCompleteCalculation('23081996'));
     }
 
     public function testDigitalRootBulk() : void {
         $this->assertEquals([
             '23081996' => '2',
             '43434336' => '3'
-        ],
-        (new digitalRootBuilder)->getdigitalRootBulk(['23081996','43434336']));
+        ], (new digitalRootBuilder)->getdigitalRootBulk(['23081996','43434336']));
     }
 
     public function testDigitalRootLetters() : void {
-        $this->assertEquals("3", (new digitalRootBuilder)->getDigitalRoot("a,..,b"));
+        $this->assertEquals([
+            'client_input' => 'a,..,b',
+            'digital_root' => '3'
+        ], (new digitalRootBuilder)->getDigitalRoot('a,..,b'));
     }
 
     public function testDigitalRootAlternativeLetters() : void {
-        $this->assertEquals("9", (new digitalRootBuilder)->getDigitalRoot("abc12", ["A" => 1, "B" => 2, "C" => 3]));
+        $this->assertEquals([
+            'client_input' => 'abc12',
+            'digital_root' => '9'
+        ], (new digitalRootBuilder)->getDigitalRoot('abc12', ['A' => 1, 'B' => 2, 'C' => 3]));
     }
 
 }
