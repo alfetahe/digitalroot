12152714d7fae385db3d2a7ae5879609d9ab1014
Anuar <anuar.ssn@gmail.com>
2020-04-14 06:20:20 +0300

    Remade the client side using static functions

--- src/digitalRootBuilder.php
+++ src/digitalRootBuilder.php
@@ -4,56 +4,55 @@ namespace digitalRootSrc;
 
 use digitalRootSrc\digitalRoot;
 
-class digitalRootBuilder {
-    public $digitalRootModel;
+class digitalRootBuilder
+{
 
-    // Builds single digitalRoot instance.
-    private function buildSingleInstance($input, $alternative_values = null) {
-
-        $this->digitalRootModel = new digitalRoot($input, $alternative_values);
+    // Returns single digitalRoot instance.
+    private static function buildSingleInstance(string $input, array $alternative_values = null): digitalRoot
+    {
+        return (new digitalRoot($input, $alternative_values));
     }
 
-    public function getDigitalRoot($input, $alternative_values = null)
+    public static function getDigitalRoot(string $input, array $alternative_values = null): array
     {
-        $this->buildSingleInstance($input, $alternative_values);
+        $digitalRootModel = digitalRootBuilder::buildSingleInstance($input, $alternative_values);
 
-        $this->digitalRootModel->shortCalculation();
+        $digitalRootModel->shortCalculation();
 
         return [
-            'client_input' => $this->digitalRootModel->getOrigInput(),
-            'digital_root' => $this->digitalRootModel->getDigRoot()
+            'client_input' => $digitalRootModel->getOrigInput(),
+            'digital_root' => $digitalRootModel->getDigRoot()
         ];
     }
 
-    public function getDigitalRootCompleteCalculation($input, $alternative_values = null) 
+    public static function getDigitalRootCompleteCalculation(string $input, array $alternative_values = null): array
     {
-        $this->buildSingleInstance($input, $alternative_values);
-        
-        $this->digitalRootModel->longCalculation();
+        $digitalRootModel = digitalRootBuilder::buildSingleInstance($input, $alternative_values);
+
+        $digitalRootModel->longCalculation();
 
         return [
-            'client_input' => $this->digitalRootModel->getOrigInput(),
-            'digital_root' => $this->digitalRootModel->getDigRoot(),
-            'digital_root_from' => $this->digitalRootModel->getDigitalRootFrom(),
-            'full_calculation' => $this->digitalRootModel->getDigRootFullCalculation(),
-            'single_digit_summaries' => $this->digitalRootModel->getSingleDigitSummaries(),
-            'double_digit_summaries' => $this->digitalRootModel->getDoubleDigitSummaries(),
-            'double_digit_summaries_separated_digits' => $this->digitalRootModel->getDigRootddsSeparated(),
-            'double_digit_summaries_separated_digits_summaries' => $this->digitalRootModel->getDigRootddssSummaries(),
+            'client_input' => $digitalRootModel->getOrigInput(),
+            'digital_root' => $digitalRootModel->getDigRoot(),
+            'digital_root_from' => $digitalRootModel->getDigitalRootFrom(),
+            'full_calculation' => $digitalRootModel->getDigRootFullCalculation(),
+            'single_digit_summaries' => $digitalRootModel->getSingleDigitSummaries(),
+            'double_digit_summaries' => $digitalRootModel->getDoubleDigitSummaries(),
+            'double_digit_summaries_separated_digits' => $digitalRootModel->getDigRootddsSeparated(),
+            'double_digit_summaries_separated_digits_summaries' => $digitalRootModel->getDigRootddssSummaries(),
         ];
     }
 
-    public function getdigitalRootBulk($values, $alternative_values = null) {
+    public function getdigitalRootBulk(array $values, array $alternative_values = null): array
+    {
         $returnData = [];
 
-        foreach($values as $value) {
-            $this->buildSingleInstance($value, $alternative_values);
-            $this->digitalRootModel->shortCalculation();
-            $returnData[$value] = $this->digitalRootModel->getDigRoot();
+        foreach ($values as $value) {
+            $digitalRootModel = digitalRootBuilder::buildSingleInstance($value, $alternative_values);
+            $digitalRootModel->shortCalculation();
+            $returnData[$value] = $digitalRootModel->getDigRoot();
         }
 
         return $returnData;
     }
 }
-
-?>
